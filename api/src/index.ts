5a661e2b4cd9de7554b09c8aae3f6f7deda132a7
t-nguy <thomasnguyen852@gmail.com>
2023-11-15 12:59:03 -0800

    cors backend fix maybe

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express, {Application} from "express";
 import routes from "./routes/routes";
+import cors from "cors";
 
 const app: Application = express();
 const PORT: number = 8080;
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
