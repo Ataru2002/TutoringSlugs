86a2cc54ba2fcee82550a0a446f0ebe3987ef21c
Phuong <phuongunguyen4@gmail.com>
2023-11-26 09:23:32 -0800

    got rid of strict mode

--- src/index.js
+++ src/index.js
@@ -8,12 +8,9 @@ import theme from './components/Theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-  //</React.StrictMode>
-  
+    <ThemeProvider theme={theme}>
+    <App />
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
